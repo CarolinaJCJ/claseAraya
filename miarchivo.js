e3379a423772bff7ad6a90efc284fc6224d80c84
CarolinaJCJ <111032921+CarolinaJCJ@users.noreply.github.com>
2022-08-21 01:27:27 -0400

    Add files via upload

---
+++ miarchivo.js
@@ -0,0 +1,13 @@
+/*for (let i = 0; i < 100; i++) {
+   console.log("hola")
+    
+}*/
+
+let usuario = prompt("Homero, aqui hay una familia de zarigueyas. A la grande le puse...?");
+while (usuario != "cuca"){
+    alert("error 404 not found");
+    usuario = prompt("Como se llama la zarigueya?");
+    
+    
+}
+alert("Felicidades!, ya puedes manejar un monoriel :D");
